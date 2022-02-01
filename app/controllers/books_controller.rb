09911d4cb6834820df31f3e87d21baacc6c6a991
njguld95038043 <t.tamura6060@gmail.com>
2022-02-01 10:39:00 +0000

    [Update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,13 +4,19 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+    flash[:success] = "Book was successfully created."
+    redirect_to book_path(@book.id)
+    else
+    @books = Book.all
+    render :index
+    end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -22,9 +28,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:success] = "Book was successfully updated."
+    redirect_to book_path(@book.id)
+    else
+    render :edit
+    end
   end
 
   def destroy
